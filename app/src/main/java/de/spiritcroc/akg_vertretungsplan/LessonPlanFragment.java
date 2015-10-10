dec380277bbff656a139b396e691b858580a74ee
SpiritCroc <spiritcroc@gmail.com>
2015-10-10 15:06:22 +0200

    LessonPlan: Add some beauty
    - Different color for time (yellow for now, setting will come later)
    - Same width for time → same x for lessons
    - Margin between time and lessons

--- app/src/main/java/de/spiritcroc/akg_vertretungsplan/LessonPlanFragment.java
+++ app/src/main/java/de/spiritcroc/akg_vertretungsplan/LessonPlanFragment.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,6 +41,8 @@ public class LessonPlanFragment extends ListFragment {
     private String[] startTimes;
     private String[] fullTimes;
     private ArrayList<Integer> textColors = new ArrayList<>();
+
+    private ArrayList<TextView> timeViews = new ArrayList<>();
 
     private SharedPreferences sharedPreferences;
     private boolean showTime;
@@ -76,6 +79,12 @@ public class LessonPlanFragment extends ListFragment {
         update();
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getListView().post(updateTimeViewWidth);
+    }
+
     private LessonViewContent[] createContent(){
         textColors.clear();
         int style = Tools.getStyle(getActivity());
@@ -85,15 +94,15 @@ public class LessonPlanFragment extends ListFragment {
         LessonViewContent[] lessonViewContent = new LessonViewContent[lessons.length];
         for (int i = 0; i < lessonViewContent.length; i++){
             lessonViewContent[i] = new LessonViewContent();
-            lessonViewContent[i].content = (showTime ? (showFullTime ? fullTimes[i] : startTimes[i]) : (i+1)) + ": ";
+            lessonViewContent[i].time = (showTime ? (showFullTime ? fullTimes[i] : startTimes[i]) : (i+1)) + ": ";
             if (lessons[i].isFreeTime()) {
                 if (i+1 == LessonPlan.LUNCH_BREAK)
-                    lessonViewContent[i].content += getString(R.string.lunch_break);
+                    lessonViewContent[i].content = getString(R.string.lunch_break);
                 else
-                    lessonViewContent[i].content += getString(R.string.free_time);
+                    lessonViewContent[i].content = getString(R.string.free_time);
                 textColors.add(Color.GRAY);
             } else {
-                lessonViewContent[i].content += lessons[i].getReadableName();
+                lessonViewContent[i].content = lessons[i].getReadableName();
                 textColors.add(lightStyle ? Color.BLACK : Color.WHITE);
                 lessonViewContent[i].room = lessons[i].getRoom();
             }
@@ -118,8 +127,31 @@ public class LessonPlanFragment extends ListFragment {
     public void update(){
         showTime = sharedPreferences.getBoolean("pref_lesson_plan_show_time", false);
         showFullTime = sharedPreferences.getBoolean("pref_lesson_plan_show_full_time", false);
+        timeViews.clear();
         setListAdapter(new CustomArrayAdapter(getActivity().getApplicationContext(), R.layout.lesson_plan_item, createContent()));
+        try {
+            getListView().post(updateTimeViewWidth);
+        } catch (Exception e) {
+            // I can't find a better way to check if listView is created
+            Log.v("LessonPlanFragment", "Cannot set time width: " + e);
+        }
     }
+
+    private Runnable updateTimeViewWidth = new Runnable() {
+        @Override
+        public void run() {
+            int maxWidth = 0;
+            for (int i = 0; i < timeViews.size(); i++) {
+                int width = timeViews.get(i).getWidth();
+                if (width > maxWidth) {
+                    maxWidth = width;
+                }
+            }
+            for (int i = 0; i < timeViews.size(); i++) {
+                timeViews.get(i).setWidth(maxWidth);
+            }
+        }
+    };
 
     public class CustomArrayAdapter extends ArrayAdapter {
         public CustomArrayAdapter (Context context, int resource, LessonViewContent[] objects){
@@ -135,6 +167,7 @@ public class LessonPlanFragment extends ListFragment {
                 view = inflater.inflate(R.layout.lesson_plan_item, parent, false);
 
                 holder = new LessonViewHolder();
+                holder.timeView = (TextView) view.findViewById(R.id.time_view);
                 holder.subjectView = (TextView) view.findViewById(R.id.subject_view);
                 holder.roomView = (TextView) view.findViewById(R.id.room_view);
 
@@ -145,8 +178,12 @@ public class LessonPlanFragment extends ListFragment {
                 view = convertView;
             }
 
+            timeViews.add(holder.timeView);
+            holder.timeView.setText(((LessonViewContent) getItem(position)).time);
             holder.subjectView.setText(((LessonViewContent) getItem(position)).content);
             holder.roomView.setText(((LessonViewContent) getItem(position)).room);
+
+            holder.timeView.setGravity(showTime && showFullTime ? Gravity.CENTER_HORIZONTAL : Gravity.CENTER_VERTICAL | Gravity.RIGHT);
 
             if (textColors.size()>position)
                 holder.subjectView.setTextColor(textColors.get(position));
@@ -157,10 +194,10 @@ public class LessonPlanFragment extends ListFragment {
     }
 
     static class LessonViewHolder{
-        TextView subjectView, roomView;
+        TextView timeView, subjectView, roomView;
     }
 
     private class LessonViewContent{
-        String content, room;
+        String time, content, room;
     }
 }
