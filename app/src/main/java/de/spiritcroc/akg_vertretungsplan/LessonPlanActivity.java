1d5105257ba984697cab1283b8b3610de3b33f59
SpiritCroc <spiritcroc@gmail.com>
2015-10-25 15:24:17 +0100

    Lesson plan: add about dialog menu entry

--- app/src/main/java/de/spiritcroc/akg_vertretungsplan/LessonPlanActivity.java
+++ app/src/main/java/de/spiritcroc/akg_vertretungsplan/LessonPlanActivity.java
@@ -125,6 +125,9 @@ public class LessonPlanActivity extends AppCompatActivity {
             case R.id.action_settings:
                 startActivity(new Intent(this, SettingsActivity.class).addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT));
                 return true;
+            case R.id.action_about:
+                new AboutDialog().show(getFragmentManager(), "AboutDialog");
+                return true;
             case R.id.action_reset:
                 new ConfirmRemoveLessonsDialog().setValues(this).show(getFragmentManager(), "ConfirmRemoveLessonsDialog");
                 return true;
