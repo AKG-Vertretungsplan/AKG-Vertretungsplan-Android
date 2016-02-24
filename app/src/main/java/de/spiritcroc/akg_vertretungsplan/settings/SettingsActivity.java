7d2fae5b0efbbe6162b264867e8c4080b8b9899e
SpiritCroc <spiritcroc@gmail.com>
2016-02-24 20:22:18 +0100

    Settings: Try to remember position when recreating
    -> When changing theme

--- app/src/main/java/de/spiritcroc/akg_vertretungsplan/settings/SettingsActivity.java
+++ app/src/main/java/de/spiritcroc/akg_vertretungsplan/settings/SettingsActivity.java
@@ -25,6 +25,8 @@ import android.preference.PreferenceScreen;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
 import de.spiritcroc.akg_vertretungsplan.IsRunningSingleton;
 import de.spiritcroc.akg_vertretungsplan.Tools;
@@ -38,6 +40,10 @@ public class SettingsActivity extends AppCompatActivity {
 
     private static final String EXTRA_PREFERENCE_FRAGMENT =
             "de.spiritcroc.akg_vertretungsplan.extra.preference_fragment";
+    private static final String EXTRA_PREFERENCE_POSITION =
+            "de.spiritcroc.akg_vertretungsplan.extra.preference_position";
+    private static final String EXTRA_PREFERENCE_POSITION_TOP =
+            "de.spiritcroc.akg_vertretungsplan.extra.preference_position_top";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +73,23 @@ public class SettingsActivity extends AppCompatActivity {
             ActionBar actionBar = getSupportActionBar();
             if (actionBar != null) {
                 actionBar.setTitle(preferenceFragment.getPreferenceScreen().getTitle());
+            }
+
+            if (getIntent().hasExtra(EXTRA_PREFERENCE_POSITION)) {
+                // Try to restore position after recreating activity
+                View v = findViewById(android.R.id.list);
+                if (v instanceof ListView) {
+                    final int pos = getIntent().getIntExtra(EXTRA_PREFERENCE_POSITION, 0);
+                    final int top = getIntent().getIntExtra(EXTRA_PREFERENCE_POSITION_TOP, 0);
+                    final ListView lv = (ListView) v;
+                    v.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            lv.setSelectionFromTop(pos, top);
+                        }
+                    });
+                }
+                getIntent().removeExtra(EXTRA_PREFERENCE_POSITION);
             }
         }
     }
@@ -109,5 +132,18 @@ public class SettingsActivity extends AppCompatActivity {
             }
         }
         return new SettingsFragment();
+    }
+
+    @Override
+    public void recreate() {
+        // Try to remember position
+        View v = findViewById(android.R.id.list);
+        if (v instanceof ListView) {
+            View child = ((ListView) v).getChildAt(0);
+            getIntent().putExtra(EXTRA_PREFERENCE_POSITION_TOP, child == null ? 0 : child.getTop())
+                    .putExtra(EXTRA_PREFERENCE_POSITION,
+                            ((ListView) v).getFirstVisiblePosition());
+        }
+        super.recreate();
     }
 }
